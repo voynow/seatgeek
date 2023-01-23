279c1cde04e6cd736fbd4fa665a524a248bfdced
voynow <voynow99@gmail.com>
2023-01-23 18:26:19 -0500

    Mocking multi-url input

--- api-lambda/lambda_function.py
+++ api-lambda/lambda_function.py
@@ -2,6 +2,11 @@ import json
 
 def lambda_handler(event, context):
 
-    return [{
-        "url": "https://seatgeek.com/nets-at-76ers-tickets/1-25-2023-philadelphia-pennsylvania-wells-fargo-center/nba/5775973",
-    }]
+    return [
+        {
+            "url": "https://seatgeek.com/nets-at-76ers-tickets/1-25-2023-philadelphia-pennsylvania-wells-fargo-center/nba/5775973",
+        },
+        {
+            "url": "https://seatgeek.com/nuggets-at-76ers-tickets/1-28-2023-philadelphia-pennsylvania-wells-fargo-center/nba/5775975",
+        }
+    ]
