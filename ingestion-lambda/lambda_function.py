cfbe05fbed9f38af5475bfba6789cfcaa80a6183
voynow <voynow99@gmail.com>
2023-01-23 17:17:51 -0500

    Add extraction functions

--- ingestion-lambda/lambda_function.py
+++ ingestion-lambda/lambda_function.py
@@ -1,5 +1,10 @@
 import json
+import utils
 
 def lambda_handler(event, context):
+    """
+    """
+    driver = utils.create_driver_helper()
+    raw_data = utils.extract(driver, event['url'])
 
-    return event['url']
+    return raw_data
